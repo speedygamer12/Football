f35892c2ee89c09897c68c7ca5b1403bbea22f1c
Valentine Ifedayo <vcaleb01@gmail.com>
2023-06-15 03:19:01 +0100

    FIX: Added URL

--- src/redux/services/Football.js
+++ src/redux/services/Football.js
@@ -3,16 +3,14 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // Define your API endpoints here
 export const footballApi = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://api.football-data.org/v4/competitions", prepareHeaders: (headers, { getState }) => {
+  baseQuery: fetchBaseQuery({ baseUrl: "https://football-preview.onrender.com", prepareHeaders: (headers, { getState }) => {
     headers.set("X-Auth-Token", "cec25609216f4f90acf135c97565d6af");
-    // headers.set("Access-Control-Allow-Methods", "GET")
-    // headers.set("Access-Control-Allow-Origin", "*")
     return headers;
 } }),
   endpoints: (builder) => ({
     // Define your endpoints here
     getLeagueTable: builder.query({
-      query: (league) => `${league}/standings`,
+      query: (league) => `standings/${league}`,
     }),
   }),
 });
